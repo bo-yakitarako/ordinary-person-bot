6ba77ce438a44ea75ce0544ee2fb8a21236a67ef
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2025-03-02 07:36:03 +0900

    やったね

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,42 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, EmbedBuilder, Events, GatewayIntentBits, TextChannel } from 'discord.js';
 import { config } from 'dotenv';
 import { parseNoharaHiroshi } from './scraping';
 
 config();
 
 const TOKEN = process.env.TOKEN;
+const GUILD_ID = process.env.GUILD_ID;
+const CHANNEL_ID = process.env.CHANNEL_ID;
 
 if (TOKEN === undefined) {
   throw new Error('TOKEN is not defined');
+}
+if (GUILD_ID === undefined) {
+  throw new Error('GUILD_ID is not defined');
+}
+if (CHANNEL_ID === undefined) {
+  throw new Error('CHANNEL_ID is not defined');
 }
 
 const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
 
 const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
 
-client.on(Events.ClientReady, (client) => {
+client.on(Events.ClientReady, async (client) => {
   console.log(`Ready! Logged in as ${client.user.tag}`);
-});
 
-client.on(Events.MessageCreate, async (message) => {
-  if (message.author.bot) {
+  const guild = client.guilds.cache.get(GUILD_ID);
+  if (guild === undefined) {
     return;
   }
-  if (message.content === '野原') {
-    await message.reply('ひろし');
+  const channel = guild.channels.cache.get(CHANNEL_ID) as TextChannel;
+  if (channel === undefined) {
+    return;
   }
+
+  const { link, imageSrc, title } = await parseNoharaHiroshi();
+  const embed = new EmbedBuilder().setTitle(title).setURL(link).setImage(imageSrc);
+  await channel.send({ embeds: [embed] });
 });
 
 client.login(TOKEN);
-
-parseNoharaHiroshi();
--- src/scraping.ts
+++ src/scraping.ts
@@ -11,5 +11,27 @@ export const parseNoharaHiroshi = async () => {
   const res = await fetch(ENTRY);
   const html = await res.text();
   const root = parse(html);
-  console.log(root.querySelector('#episodes'));
+  const latestEpisode = root.querySelector('#episodes .row:first-child');
+  if (latestEpisode === null) {
+    throw new Error('Latest episode not found');
+  }
+  const link = latestEpisode.querySelector('a')?.getAttribute('href');
+  if (!link) {
+    throw new Error('Latest episode link not found');
+  }
+  const imageSrc = latestEpisode
+    .querySelector('.item__list-img figure a:last-child img')
+    ?.getAttribute('src');
+  if (!imageSrc) {
+    throw new Error('Latest episode image source not found');
+  }
+  const title = latestEpisode.querySelector('.item__list-text h3 a')?.innerText;
+  if (!title) {
+    throw new Error('Latest episode title not found');
+  }
+  return {
+    link,
+    imageSrc,
+    title: title.trim(),
+  };
 };
