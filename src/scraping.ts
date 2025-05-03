3f359d36af1490501a1dbab3c447c950616ef7b2
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2025-05-03 16:20:03 +0900

    うひょ

--- src/scraping.ts
+++ src/scraping.ts
@@ -5,35 +5,38 @@ config();
 
 const debug = process.env.DEBUG === 'true';
 
-const ENTRY = debug
-  ? 'http://localhost:8080'
-  : 'https://manga-shinchan.com/hiroshi-hirumeshi-saimaru';
+const ENTRY = debug ? 'http://localhost:8080' : 'https://manga-shinchan.com/hiroshi-hirumeshi';
 
 export const parseNoharaHiroshi = async () => {
   const res = await fetch(ENTRY);
   const html = await res.text();
   const root = parse(html);
-  const latestEpisode = root.querySelector('#episodes .row:first-child');
-  if (latestEpisode === null) {
-    throw new Error('Latest episode not found');
+  for (const episode of root.querySelectorAll('#episodes .row')) {
+    if (episode === null) {
+      continue;
+    }
+    if (episode.querySelector('.item__list-text p')?.innerText.includes('更新予定')) {
+      continue;
+    }
+    const link = episode.querySelector('a')?.getAttribute('href');
+    if (!link) {
+      throw new Error('エピソードリンクが見つからんぽい');
+    }
+    const imageSrc = episode
+      .querySelector('.item__list-img figure a:last-child img')
+      ?.getAttribute('src');
+    if (!imageSrc) {
+      throw new Error('エピソードはあるけどサムネ画像が見つからないらしい');
+    }
+    const title = episode.querySelector('.item__list-text h3 a')?.innerText;
+    if (!title) {
+      throw new Error('エピソードはあるけどタイトルが見つからないらしい');
+    }
+    return {
+      link,
+      imageSrc,
+      title: title.trim(),
+    };
   }
-  const link = latestEpisode.querySelector('a')?.getAttribute('href');
-  if (!link) {
-    throw new Error('Latest episode link not found');
-  }
-  const imageSrc = latestEpisode
-    .querySelector('.item__list-img figure a:last-child img')
-    ?.getAttribute('src');
-  if (!imageSrc) {
-    throw new Error('Latest episode image source not found');
-  }
-  const title = latestEpisode.querySelector('.item__list-text h3 a')?.innerText;
-  if (!title) {
-    throw new Error('Latest episode title not found');
-  }
-  return {
-    link,
-    imageSrc,
-    title: title.trim(),
-  };
+  throw new Error('根本的になにかが違う');
 };
